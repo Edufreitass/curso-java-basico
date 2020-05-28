c551aef5dab30cf56401dd2483115df552d9753b
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-05-28 12:23:39 -0300

    Aula 10 - Introdução a Variáveis

---
+++ com/loiane/cursojava/aula10/IntroducaoAVariaveis.java
@@ -0,0 +1,46 @@
+package com.loiane.cursojava.aula10;
+
+public class IntroducaoAVariaveis {
+	// Conceitos
+	
+	/**
+	 * O que é Variável?
+	 * - Área de memória associada a um nome, que pode armazenar valores
+	 * de um determinado tipo, exemplo: nome da pessoa, idade, altura etc..
+	 * 
+	 * Como declarar uma variável em Java
+	 * - <tipo> <nome da variaveL>;
+	 * - <tipo> <nome da variavel> = <valor>; // fazendo a inicializaçao da variavel
+	 * 
+	 * Convenção de Nomenclatura de uma Variavel
+	 * - a-z  |  A-z  |  _  |  &
+	 * - a-z  |  A-z  |  _  |  &  |  0-9
+	 * - Case Sensitive(camelCase), por convenção nome de variavel, começa com letra minuscula
+	 * 
+	 * Algumas Palavras Reservadas do Java:
+	 * - abstract	| - do			| - implements		| - return		| - while		
+	 * - boolean	| - double		| - import			| - short			
+	 * - break		| - else		| - int				| - static			
+	 * - byte		| - enum		| - interface		| - super			
+	 * - case		| - extends		| - long			| - switch			
+	 * - catch		| - final		| - new				| - this			
+	 * - char		| - finally		| - package			| - throw
+	 * - class		| - float		| - private			| - throws
+	 * - continue	| - for			| - protected		| - try
+	 * - default	| - if			| - public			| - void											
+	 * - Obs: Existem diversas outras palavras essas são apenas algumas que aparecem mais no dia dia...
+	 * 
+	 * Tipos Primitivos
+	 * - int 		-> 1, 0, -1 
+	 * - float 		-> 1.2, 3.0, 0.8
+	 * - char 		-> A, B, C
+	 * - boolean	-> true e false
+	 * 
+	 * Convenção de Pacotes(Packages) em Java
+	 * - Tudo em letra minuscula
+	 * - Os espaços sao substituidos por "." pontos
+	 * - Padrao de nomenclatura, geralmente começa pelo domínio da empresa
+	 * - Exemplo: br.com.google
+	 */
+	
+}
