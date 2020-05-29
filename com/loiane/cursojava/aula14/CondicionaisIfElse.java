473aa394dc9e622352a96cf71e4c6956cef65b17
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-05-29 15:07:24 -0300

    Aula 14 - Condicionais If-Else

---
+++ com/loiane/cursojava/aula14/CondicionaisIfElse.java
@@ -0,0 +1,36 @@
+package com.loiane.cursojava.aula14;
+
+public class CondicionaisIfElse {
+
+	// Conceitos
+	
+	/**
+	 * O que é controle de decisão?
+	 * IF (se)
+	 * IF - ELSE (se - senão)
+	 * Múltiplos if - else
+	 * 
+	 * Comando IF
+	 * - Comandos dentro de um bloco IF somente são executados se a condição for verdadeira.
+	 * - Se a condição for falsa, os comandos não são executados.
+	 * 
+	 * Exemplo:
+	 * 		if (idade >= 18) {
+	 * 			System.out.println("É maior de idade");
+	 * 		}
+	 * 
+	 * Comando IF - ELSE
+	 * - Comandos dentro de um bloco ELSE somente são executados se a condição do IF for falsa.
+	 * 
+	 * Exemplo:
+	 * 		if (idade >= 18) {
+	 * 			System.out.println("É maior de idade");
+	 * 		} else {
+	 * 			System.out.println("Não é maior de idade!);
+	 * 		}
+	 * 
+	 * Múltiplos IF - ELSE
+	 * - É possível ter vários if-else aninhados!
+	 */
+	
+}
