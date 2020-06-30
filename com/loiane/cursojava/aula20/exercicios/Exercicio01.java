a7ee53341999d8858cd21c170df2e28ab40fc972
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-06-30 18:53:47 -0300

    Aula 20 - Correção Exercícios (Arrays Multidimensionais) - Parte 1

--- com/loiane/cursojava/aula20/exercicios/Exercicio01.java
+++ com/loiane/cursojava/aula20/exercicios/Exercicio01.java
@@ -1,5 +1,7 @@
 package com.loiane.cursojava.aula20.exercicios;
 
+import java.util.Random;
+
 public class Exercicio01 {
 
 	/*
@@ -7,9 +9,42 @@ public class Exercicio01 {
 	 Após isso determine o maior número da matriz e a sua posição (linha, coluna).
 	 */
 	public static void main(String... args) {
-		
-		
-		
+
+		int[][] numerosAleatorios = new int[4][4];
+
+		Random numeroRandom = new Random();
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				numerosAleatorios[i][j] = numeroRandom.nextInt(100);
+			}
+		}
+
+		int maior = 0; // Integer.MIN_VALUE
+		int linha = 0;
+		int col = 0;
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				if (numerosAleatorios[i][j] > maior) {
+					maior = numerosAleatorios[i][j];
+					linha = i;
+					col = j;
+				}
+			}
+		}
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				System.out.print(numerosAleatorios[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("Maior valor = " + maior);
+		System.out.println("Linha = " + linha);
+		System.out.println("Coluna = " + col);
+
 	}
-	
+
 }
--- com/loiane/cursojava/aula20/exercicios/Exercicio03.java
+++ com/loiane/cursojava/aula20/exercicios/Exercicio03.java
@@ -1,5 +1,7 @@
 package com.loiane.cursojava.aula20.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
 
 	/*
@@ -8,9 +10,43 @@ public class Exercicio03 {
 	a quantidade de números pares, a quantidade de números ímpares. 
 	 */
 	public static void main(String... args) {
-		
-		
-		
+
+		Scanner scan = new Scanner(System.in);
+
+		int[][] numeros = new int[3][3];
+
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; j++) {
+				System.out.println("Entre com o valor da posição [" + i + "," + j + "]");
+				numeros[i][j] = scan.nextInt();
+			}
+		}
+
+		int qtdPares = 0;
+		int qtdImpares = 0;
+
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; j++) {
+				if (numeros[i][j] % 2 == 0) {
+					qtdPares++;
+				} else {
+					qtdImpares++;
+				}
+			}
+		}
+
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; j++) {
+				System.out.print(numeros[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("Pares = " + qtdPares);
+		System.out.println("Impares = " + qtdImpares);
+
+		scan.close();
+
 	}
-	
+
 }
