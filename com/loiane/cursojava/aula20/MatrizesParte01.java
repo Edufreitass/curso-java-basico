288e09682881f7d6525606249fb259a8fd633103
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-06-30 16:43:44 -0300

    Ajustes na estrutura do MatrizesParte01

---
+++ com/loiane/cursojava/aula20/MatrizesParte01.java
@@ -0,0 +1,31 @@
+package com.loiane.cursojava.aula20;
+
+public class MatrizesParte01 {
+
+	// Parte 1: Introdução e Prática
+	public static void main(String[] args) {
+
+		// 3 alunos e 4 notas
+		double[][] notasAlunos = new double[3][4];
+
+		// 1° aluno
+		notasAlunos[0][0] = 10;
+		notasAlunos[0][1] = 7;
+		notasAlunos[0][2] = 9;
+		notasAlunos[0][3] = 9.5;
+
+		// 2° aluno
+		notasAlunos[1][0] = 9;
+		notasAlunos[1][1] = 8;
+		notasAlunos[1][2] = 7;
+		notasAlunos[1][3] = 9;
+
+		// 3° aluno
+		notasAlunos[2][0] = 8;
+		notasAlunos[2][1] = 9;
+		notasAlunos[2][2] = 10;
+		notasAlunos[2][3] = 7;
+
+	}
+
+}
