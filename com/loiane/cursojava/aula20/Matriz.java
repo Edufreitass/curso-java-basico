bb3da26cab7491caf4b59088088f8a747c204da2
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-06-30 16:44:18 -0300

    Ajustes na Classe de conceito Matriz

--- com/loiane/cursojava/aula20/Matriz.java
+++ com/loiane/cursojava/aula20/Matriz.java
@@ -1,11 +1,10 @@
 package com.loiane.cursojava.aula20;
 
-public class Matrizes {
+public class Matriz {
 
 	// Conceitos
 	
 	// Parte 1: Introdução e Prática
-	
 	// Parte 2: Manipulação
 	// Parte 3: Matrizes 3 dimensões
 	// Parte 4: Matrizes Irregulares
@@ -55,30 +54,9 @@ public class Matrizes {
 	 *  notasAlunos[1][1] = 8;
 	 *  notasAlunos[1][2] = 7;
 	 *  notasAlunos[1][3] = 9;
+	 *  
+	 *  - Array de arrays
+	 *  double[][] notasAlunos2 = {{ 10, 7, 8, 9.5}, {9, 8, 7, 9}};
 	 */
-	
-	public static void main(String[] args) {
-		
-		// 30 alunos e 4 notas
-		double[][] notasAlunos = new double[30][4];
-		
-		// 1° aluno
-		notasAlunos[0][0] = 10;
-		notasAlunos[0][1] = 7;
-		notasAlunos[0][2] = 9;
-		notasAlunos[0][3] = 9.5;
-		
-		// 2° aluno
-		notasAlunos[1][0] = 9;
-		notasAlunos[1][1] = 8;
-		notasAlunos[1][2] = 7;
-		notasAlunos[1][3] = 9;
-		
-		// 3° aluno
-		notasAlunos[2][0] = 8;
-		notasAlunos[2][1] = 9;
-		notasAlunos[2][2] = 10;
-		notasAlunos[2][3] = 7;
-	}
 	
 }
