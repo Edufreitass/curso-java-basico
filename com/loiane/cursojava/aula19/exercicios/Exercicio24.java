b4181245e5013d54092e979866a6d768a8eaa636
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-06-30 11:20:22 -0300

    Aula 19 - Correção Exercícios(Arrays) - Parte 4

--- com/loiane/cursojava/aula19/exercicios/Exercicio24.java
+++ com/loiane/cursojava/aula19/exercicios/Exercicio24.java
@@ -1,5 +1,7 @@
 package com.loiane.cursojava.aula19.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio24 {
 
 	/*
@@ -14,8 +16,42 @@ public class Exercicio24 {
 	 */
 	public static void main(String[] args) {
 
-		
-		
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com um numero para a posição " + i);
+			vetorA[i] = scan.nextInt();
+		}
+
+		// 1221
+		// i = 2
+
+		// flag
+		boolean palindromo = true;
+		for (int i = 0; i < (vetorA.length / 2); i++) {
+			if (vetorA[i] != vetorA[vetorA.length - 1 - i]) {
+				palindromo = false;
+				break;
+			}
+		}
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+
+		System.out.println();
+
+		if (palindromo) {
+			System.out.println("Palindromo");
+		} else {
+			System.out.println("Não é palindromo");
+		}
+
+		scan.close();
+
 	}
-	
+
 }
--- com/loiane/cursojava/aula19/exercicios/Exercicio26.java
+++ com/loiane/cursojava/aula19/exercicios/Exercicio26.java
@@ -1,5 +1,7 @@
 package com.loiane.cursojava.aula19.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio26 {
 
 	/*
@@ -11,8 +13,53 @@ public class Exercicio26 {
 	 */
 	public static void main(String[] args) {
 
-		
-		
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		int[] vetorC = new int[vetorB.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com um número para a posição A = " + i);
+			vetorA[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.println("Entre com um número para a posição B = " + i);
+			vetorB[i] = scan.nextInt();
+
+			if (vetorA[i] == vetorB[i]) {
+				vetorC[i] = 0;
+			} else if (vetorA[i] > vetorB[i]) {
+				vetorC[i] = 1;
+			} else {
+				vetorC[i] = -1;
+			}
+		}
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.print(vetorB[i] + " ");
+		}
+
+		System.out.println();
+
+		System.out.print("Vetor C = ");
+		for (int i = 0; i < vetorC.length; i++) {
+			System.out.print(vetorC[i] + " ");
+		}
+
+		System.out.println();
+
+		scan.close();
+
 	}
-	
+
 }
