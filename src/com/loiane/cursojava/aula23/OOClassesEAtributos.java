024726fb7bf2b43d698b341d4e8f31ec12dcb1b5
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-01 17:10:58 -0300

    Aula 24 - Orientação a Objetos: Classes e atributos

---
+++ src/com/loiane/cursojava/aula23/OOClassesEAtributos.java
@@ -0,0 +1,68 @@
+package com.loiane.cursojava.aula23;
+
+public class OOClassesEAtributos {
+
+	// Conceitos
+	/**
+	 * Agenda
+	 * - Forma Geral
+	 * - Definição de classes
+	 * - Atributos
+	 * - Criação de Objetos (Instanciação)
+	 * 
+	 * Forma geral de uma classe:
+			class NomeDaClasse {
+			
+				// variáveis de instância - atributos
+				int var1;
+				int var2;
+				int var3;
+				
+				// declaração de métodos
+				void metodo1 (int parametros) {
+					// corpo do método
+				}
+				
+				void metodo2 (int parametros) {
+					// corpo do método
+				}
+				
+				void metodo3 (int parametros) {
+					// corpo do método
+				}
+			
+			}
+			
+	 *
+	 * Objeto = Classe
+	 * Qualidades, características = Atributos
+	 * Ações = Métodos
+	 * 
+	 * Objeto: CARRO
+	 * ATRIBUTOS: Cor, Marca, Modelo, Número de passageiros
+	 * Capacidade do tanque de combustível, Consumo de
+	 * combustível por km.
+	 * 
+	 * - Definição da classe Carro e seus atributos:
+		class Carro {
+			
+			String marca;
+			String modelo;
+			int numPassageiros; // número de passageiros
+			double capCombustivel; // capacidade do tanque de combustível
+			double consumoCombustivel; // consumo de combustível por km
+			
+		}
+	 * 
+	 * - Criação de objetos
+	 * 
+	 * Carro van = new Carro(); // Instanciando a classe Carro
+	 * var.marca = "Fiat";
+	 * var.modelo = "Ducato";
+	 * var.numPassageiros = 10;
+	 * var.capCombustivel = 100;
+	 * var.consumoCombustivel = 0.2;
+	 * 
+	 */
+	
+}
