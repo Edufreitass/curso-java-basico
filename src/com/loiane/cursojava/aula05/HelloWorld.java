9d841c25d4b9b8d94f73b4b8f9339babf6394657
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-01 15:56:04 -0300

    Aula 05 - Primeiro programa em Java

--- src/com/loiane/cursojava/aula05/HelloWorld.java
+++ src/com/loiane/cursojava/aula05/HelloWorld.java
@@ -3,7 +3,7 @@ package com.loiane.cursojava.aula05;
 // class = Declaração da Classe
 // HelloWorld = Nome da Classe
 public class HelloWorld {
-	
+
 	// Método(Função) "main", ponto de entrada dos programas Java
 	// public static = Modificadores de Acesso ao Método
 	// void = Tipo de Retorno do Método
@@ -12,10 +12,9 @@ public class HelloWorld {
 	// System.out.println("Hello World); = Nosso código
 	// { } = Blocos estão entre {} (chaves)
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello World");
-		
+
 	}
 
 }
-
