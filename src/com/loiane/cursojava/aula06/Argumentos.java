df51f46a0366a627c90791e720324779be63ce60
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-01 15:56:46 -0300

    Aula 06 - Passando argumentos para o programa

--- src/com/loiane/cursojava/aula06/Argumentos.java
+++ src/com/loiane/cursojava/aula06/Argumentos.java
@@ -3,13 +3,13 @@ package com.loiane.cursojava.aula06;
 public class Argumentos {
 
 	public static void main(String[] args) {
-		
+
 		// Java é uma linguagem compilada e interpretada
-		
+
 		// Passar argumentos através do CMD
 		// java Argumentos.java <digitar_aqui>
 		System.out.println("Voce digitou " + args[0]);
-		
+
 	}
 
 }
