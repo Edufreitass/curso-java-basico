c0b9e32b1e2fc02a5d2ae94036ddd10138b045db
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-01 15:58:54 -0300

    Aula 10 - Introdução a Variáveis

--- src/com/loiane/cursojava/aula10/Variaveis.java
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -3,9 +3,9 @@ package com.loiane.cursojava.aula10;
 public class Variaveis {
 
 	public static void main(String[] args) {
-		
+
 		// Declaração de variaveis
-		
+
 		// int = numero inteiro
 		int idade;
 		int idade1 = 20;
@@ -14,27 +14,27 @@ public class Variaveis {
 		String nome1 = "Eduardo";
 		String nomeDoMeuCachorro = "Feroz";
 		String ano2020 = "2020";
-		
+
 		idade1 = 23;
 
 		System.out.println("Idade = " + idade1);
 		System.out.println("Nome = " + nome1);
-		
+
 		// aceito, mas não utilizado
 		int _idade;
 		int $idade;
-		
+
 		// não é convenção Java
 		String nome_do_meu_cachorro;
 		String NomeDoMeuCachorro;
 		String nomeDoMeucachorro;
-		
+
 		// má prática
 		int a = 10;
 		String b = "Loiane";
 		// O que "a" significa? E "b"? Não sei o que isso significa.
 		// É recomendado sempre dar nomes as variaveis, para que seja fácil a leitura do código.
-		
+
 	}
 
 }
