17794eabd6e5030cb699ba7358f6009f924e8c3d
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-02 17:56:46 -0300

    Aula 27 - Orientação a Objetos: Correção Exercícios

---
+++ src/com/loiane/cursojava/aula27/exercicios/ex01/TestaLampada.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava.aula27.exercicios.ex01;
+
+public class TestaLampada {
+
+	/*
+	 * 1) Escreva uma classe para representar uma lâmpada. Desenvolva métodos para
+	 * ligar, desligar a lampada.
+	 */
+	public static void main(String[] args) {
+
+		Lampada lampada = new Lampada();
+
+		lampada.ligar();
+
+		lampada.mostrarEstado();
+
+		lampada.desligar();
+
+		lampada.mostrarEstado();
+
+		lampada.mudarEstado();
+
+		lampada.mostrarEstado();
+
+	}
+
+}
