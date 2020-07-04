9ad0411bd145176b0fc993149725cf5952a0a4fa
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-04 12:34:07 -0300

    Aula 32 - Orientação a Objetos: Encapsulamento: métodos getters e setters

---
+++ src/com/loiane/cursojava/aula32/TestaCarro.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula32;
+
+public class TestaCarro {
+
+	public static void main(String... args) {
+		
+		Carro van = new Carro();
+		van.setMarca("Fiat");
+		
+		System.out.println(van.getMarca());
+		
+	}
+	
+}
