3fd59607072605d10f151b3091913335d94b6f3f
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-04 02:15:06 -0300

    Aula 30 - Orientação a Objetos: Palavra chave this

---
+++ src/com/loiane/cursojava/aula30/TestaCarro.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula30;
+
+public class TestaCarro {
+
+	public static void main(String[] args) {
+
+		Carro van = new Carro("Fiat", "Ducatu");
+
+		System.out.println(van.numPassageiros);
+
+	}
+
+}
