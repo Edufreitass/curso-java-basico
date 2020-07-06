9d624e1c7bc6930ebca48258ba82f97d5b2ab38c
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-06 17:21:43 -0300

    Aula 39 - Orientação a Objetos: Herança: modificadores de acesso + protected

---
+++ src/com/loiane/cursojava/aula39/Teste.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula39;
+
+public class Teste {
+
+	// Modificador de Acesso
+	// public - TODOS tem acesso
+	// default -  Somente classes do MESMO PACOTE tem acesso
+	// private - Somente dentro da PRÓPRIA classe tem acesso
+	// protected - Parecido com default, porem as SUBCLASSES tem acesso ao atributo ou metodo
+	
+}
