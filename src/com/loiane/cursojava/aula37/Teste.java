1d86a89233ad1818e674aabacd737b880f6b69b8
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-06 16:44:20 -0300

    Aula 37 - Orientação a Objetos: Herança

---
+++ src/com/loiane/cursojava/aula37/Teste.java
@@ -0,0 +1,22 @@
+package com.loiane.cursojava.aula37;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		/* Aluno aluno = new Aluno();
+		
+		Professor prof = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		aluno.setNome("Dudu"); */
+		
+		Pessoa aluno = new Aluno();
+		// aluno.get
+		
+		Pessoa professor = new Professor();
+		// aluno.get
+	}
+	
+}
