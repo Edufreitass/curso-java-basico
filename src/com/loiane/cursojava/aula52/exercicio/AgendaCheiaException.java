ef132d04d4faa445af93e1cc4c4e49a4c869d22e
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-08 12:55:47 -0300

    Aula 52 - Exceptions: Correção de Exercícios

---
+++ src/com/loiane/cursojava/aula52/exercicio/AgendaCheiaException.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula52.exercicio;
+
+public class AgendaCheiaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage() {
+		return "Agenda já está cheia";
+	}
+
+}
